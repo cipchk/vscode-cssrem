ba13434a62455ffca2a8ea7afca5dc54f832681d
cipchk <cipchk@qq.com>
2018-11-27 12:11:08 +0800

    chore: fix code styles

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,45 @@
-'use strict';
 import * as vscode from 'vscode';
 import { CssRemProcess } from './process';
 import { CssRemProvider } from './provider';
 
 let cog = null;
 export function activate(context: vscode.ExtensionContext) {
+  cog = vscode.workspace.getConfiguration('cssrem');
 
-    cog = vscode.workspace.getConfiguration('cssrem');
+  const process = new CssRemProcess(cog);
+  let provider = new CssRemProvider(process);
+  const LANS = ['html', 'vue', 'css', 'less', 'scss', 'sass', 'stylus'];
+  for (let lan of LANS) {
+    let providerDisposable = vscode.languages.registerCompletionItemProvider(
+      lan,
+      provider
+    );
+    context.subscriptions.push(providerDisposable);
+  }
 
-    const process = new CssRemProcess(cog);
-    let provider = new CssRemProvider(process);
-    const LANS = ['html', 'vue', 'css', 'less', 'scss', 'sass', 'stylus'];
-    for (let lan of LANS) {
-        let providerDisposable = vscode.languages.registerCompletionItemProvider(lan, provider);
-        context.subscriptions.push(providerDisposable);
-    }
-
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand('extension.cssrem', (textEditor, edit) => {
+  context.subscriptions.push(
+    vscode.commands.registerTextEditorCommand(
+      'extension.cssrem',
+      (textEditor, edit) => {
         const doc = textEditor.document;
         let selection: vscode.Selection | vscode.Range = textEditor.selection;
         if (selection.isEmpty) {
-            const start = new vscode.Position(0, 0);
-            const end = new vscode.Position(doc.lineCount - 1, doc.lineAt(doc.lineCount - 1).text.length);
-            selection = new vscode.Range(start, end);
+          const start = new vscode.Position(0, 0);
+          const end = new vscode.Position(
+            doc.lineCount - 1,
+            doc.lineAt(doc.lineCount - 1).text.length
+          );
+          selection = new vscode.Range(start, end);
         }
-        
+
         let text = doc.getText(selection);
         textEditor.edit(builder => {
-            builder.replace(selection, process.convertAll(text));
+          builder.replace(selection, process.convertAll(text));
         });
-    }));
+      }
+    )
+  );
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
--- src/process.ts
+++ src/process.ts
@@ -1,49 +1,48 @@
 export class CssRemProcess {
+  constructor(private cog: any) {}
 
-    constructor(private cog: any) { }
+  private rePx: RegExp = /([-]?[\d.]+)p(x)?/;
 
-    private rePx: RegExp = /([-]?[\d.]+)p(x)?/;
+  private rePxAll: RegExp = /([-]?[\d.]+)px/g;
 
-    private rePxAll: RegExp = /([-]?[\d.]+)px/g;
-
-    /**
-     * 换px转换成rem
-     * 
-     * @private
-     * @param {string} pxStr 
-     */
-    private pxToRem(pxStr: string) {
-        const px = parseFloat(pxStr);
-        let remValue: number | string = +(px / this.cog.rootFontSize).toFixed(this.cog.fixedDigits);
-        if (this.cog.autoRemovePrefixZero) {
-            if (remValue.toString().startsWith('0.'))
-                remValue = remValue.toString().substring(1);
-        }
-        return { px: pxStr, pxValue: px, remValue, rem: remValue + 'rem'  };
+  /**
+   * 换px转换成rem
+   */
+  private pxToRem(pxStr: string) {
+    const px = parseFloat(pxStr);
+    let remValue: number | string = +(px / this.cog.rootFontSize).toFixed(
+      this.cog.fixedDigits
+    );
+    if (this.cog.autoRemovePrefixZero) {
+      if (remValue.toString().startsWith('0.'))
+        remValue = remValue.toString().substring(1);
     }
+    return { px: pxStr, pxValue: px, remValue, rem: remValue + 'rem' };
+  }
 
-    /**
-     * px转rem
-     * 
-     * @param {string} text 需要转换文本，例如：10px 12p
-     * @return {Object} { px: '10px', pxValue: 10, rem: '1rem', remValue: 1 }
-     */
-    convert(text: string) {
-        let match = text.match(this.rePx);
-        if (!match) return null;
+  /**
+   * px转rem
+   *
+   * @param {string} text 需要转换文本，例如：10px 12p
+   * @return {Object} { px: '10px', pxValue: 10, rem: '1rem', remValue: 1 }
+   */
+  convert(text: string) {
+    let match = text.match(this.rePx);
+    if (!match) return null;
 
-        return this.pxToRem(match[1]);
-    }
+    return this.pxToRem(match[1]);
+  }
 
-    /** 批量转换 */
-    convertAll(code: string): string {
-        if (!code) return code;
+  /** 批量转换 */
+  convertAll(code: string): string {
+    if (!code) return code;
 
-        return code.replace(this.rePxAll, (word: string) => {
-            const res = this.pxToRem(word);
-            if (res) return res.rem;
-            return word;
-        });
-
-    }
+    console.log(code);
+    debugger;
+    return code.replace(this.rePxAll, (word: string) => {
+      const res = this.pxToRem(word);
+      if (res) return res.rem;
+      return word;
+    });
+  }
 }
